701bf6220a896b170c32f181db0bcf52dafbf9c4
Bozhidar Stevanoski <stevanoski.bozidar@gmail.com>
2017-08-31 16:29:47 +0200

    CRUD operations for needs

---
+++ Final-Social-Cases-Project/Controllers/NeedController.cs
@@ -0,0 +1,60 @@
+﻿using Final_Social_Cases_Project.Models;
+using Final_Social_Cases_Project.Models.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Final_Social_Cases_Project.Controllers
+{
+    public class NeedController : Controller
+    {
+        public ActionResult Index() {
+            NeedContext needContext = new NeedContext();
+            List<Need> needs = needContext.Needs.ToList();
+            return View(needs);
+        }
+
+        [HttpGet]
+        public ActionResult Create() {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Need need) {         
+            if (ModelState.IsValid) {
+                NeedContext needContext = new NeedContext();
+                needContext.AddNeed(need);
+                return RedirectToAction("Index");
+            }
+            return View(need);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int needID) {
+            NeedContext needContext = new NeedContext();
+            Need need = needContext.Needs.Single(n => n.NeedID == needID);
+            return View(need);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Need need) {
+            if (ModelState.IsValid) {
+                NeedContext needContext = new NeedContext();
+                needContext.EditNeed(need);
+
+                return RedirectToAction("Index");
+            }
+            return View(need);
+        }
+
+        public ActionResult Delete(int id) {
+            NeedContext needContext = new NeedContext();
+            needContext.DeleteNeed(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Final-Social-Cases-Project/Models/Contexts/NeedContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Final_Social_Cases_Project.Models.Contexts {
+    public class NeedContext :DbContext {
+        public DbSet<Need> Needs { get; set; }
+
+        public void AddNeed(Need need) {
+
+            string connectionString = ConfigurationManager.ConnectionStrings["NeedContext"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connectionString)) {
+
+                string sql_query = "INSERT INTO Needs (NeedName)" +
+                    " VALUES (@NeedName)";
+                SqlCommand cmd = new SqlCommand(sql_query, con);
+
+                cmd.Parameters.AddWithValue("@NeedName", need.NeedName);
+                
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void EditNeed(Need need) {
+            string connectionString = ConfigurationManager.ConnectionStrings["NeedContext"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connectionString)) {
+
+                string sql_query = "UPDATE Needs" +
+                    " SET NeedName=@NeedName" +
+                    " WHERE NeedID=@NeedID";
+                SqlCommand cmd = new SqlCommand(sql_query, con);
+
+                cmd.Parameters.AddWithValue("@NeedName", need.NeedName);
+                cmd.Parameters.AddWithValue("@NeedID", need.NeedID);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteNeed(int id) {
+            string connectionString = ConfigurationManager.ConnectionStrings["NeedContext"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connectionString)) {
+
+                string sql_query = "DELETE FROM Needs" +
+                    " WHERE NeedID=@NeedID";
+                SqlCommand cmd = new SqlCommand(sql_query, con);
+
+                cmd.Parameters.AddWithValue("@NeedID", id);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
